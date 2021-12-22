b0e0059b080c456d57703d2b524e397d27ccf6a4
JGawlik12 <76133527+JGawlik12@users.noreply.github.com>
2021-12-22 22:18:41 +0100

    Add files via upload

---
+++ 5_Unit testing/Unit test development/__main__.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestGrayCodeProperties(unittest.TestCase):
+
+    def testSingleBitChange(self):
+     """Check that only one bit changes in successive codewords."""
+
+
+
+    def testUniqueCodeWords(self):
+    """Check that all codewords occur exactly once."""
+
+
+
---
+++ 5_Unit testing/Unit test development/bin2gray.py
@@ -0,0 +1,13 @@
+from myhdl import block
+
+@block
+def bin2gray(B, G):
+    # DUMMY PLACEHOLDER
+    """ Gray encoder.
+
+    B -- binary input
+    G -- Gray encoded output
+    """
+    pass
+
+
